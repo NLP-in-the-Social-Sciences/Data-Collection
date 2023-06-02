b6adcb3aea50fd585759b573d664ffcf9a30b7ee
MoRevolution <morevolution12@gmail.com>
2023-06-02 17:21:27 -0500

    Moved Files Around

---
+++ src/loader_writer_scripts.py
@@ -0,0 +1,13 @@
+import json
+
+def load_json_data(file: str) -> dict:
+    with open (file, "r", encoding="utf-8") as f:
+        data = json.load(f) 
+    return (data)
+
+
+def write_json_data(file: str, data: dict):
+    with open (file, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+
+
